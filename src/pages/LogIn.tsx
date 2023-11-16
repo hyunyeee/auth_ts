99beaf8eb146cb27a05c56d51bccbd3db3cbdc29
hyunyeee <5004ohy@naver.com>
2023-11-17 02:17:03 +0900

    feat: 로그인 폼 구현
    로그인 폼 제출 시 UserList 페이지로 리다이렉션 및 로그인 안내 alert 창을 띄운다.

--- src/pages/LogIn.tsx
+++ src/pages/LogIn.tsx
@@ -1,5 +1,18 @@
+import { useNavigate } from 'react-router-dom';
+
 const LogIn = () => {
-  return <>로그인</>;
+  const navigate = useNavigate();
+  const onSubmit = () => {
+    navigate('/');
+    alert('로그인에 성공하였습니다.');
+  };
+  return (
+    <form onSubmit={onSubmit}>
+      <input type="text" placeholder="Email" />
+      <input type="password" placeholder="Password" />
+      <button type="submit" />
+    </form>
+  );
 };
 
 export default LogIn;
