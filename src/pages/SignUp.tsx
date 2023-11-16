f92db63cee0dfbf4b9f960dac5d9eae227ea0914
hyunyeee <5004ohy@naver.com>
2023-11-17 02:20:03 +0900

    feat: 회원가입 폼 구현
    회원가입 폼 제출 후 로그인 페이지로 redirection 및 유저 생성 안내 alert창을 띄운다.

--- src/pages/SignUp.tsx
+++ src/pages/SignUp.tsx
@@ -1,5 +1,19 @@
+import { useNavigate } from 'react-router-dom';
+
 const SignUp = () => {
-  return <>회원가입</>;
+  const navigate = useNavigate();
+  const onSubmit = () => {
+    navigate('/log-in');
+    alert('유저가 성공적으로 생성되었습니다.');
+  };
+  return (
+    <form onSubmit={onSubmit}>
+      <input type="text" placeholder="Email" />
+      <input type="password" placeholder="Password" />
+      <input type="text" placeholder="Username" />
+      <button type="submit" />
+    </form>
+  );
 };
 
 export default SignUp;
